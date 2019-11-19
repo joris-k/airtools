63326f87e309d975b4792efe727f9f1750f245fa
Otto Jonetzki <ottojonetzki@gmail.com>
2019-11-19 11:10:05 +0100

    still working on validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :tool
+
+  validates :confirmation, acceptance: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/tool.rb
+++ app/models/tool.rb
@@ -1,3 +1,5 @@
 class Tool < ApplicationRecord
   belongs_to :user
+
+  validates :name, :long, :lat, :date, :price, :category, :sub_category, :description, :created_at, :updated_at, :user_id, presence: true
 end
