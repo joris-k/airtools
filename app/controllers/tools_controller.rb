c1e72411e1a54c64ad4e6a43512580926e8f47d6
Joris Kuehnast <joris@kuehnast.one>
2019-11-19 11:30:26 +0100

    adding tool is fixed

--- app/controllers/tools_controller.rb
+++ app/controllers/tools_controller.rb
@@ -12,6 +12,7 @@ class ToolsController < ApplicationController
 
   def create
     @tool = Tool.new(tool_params)
+    @tool.user = current_user
     if @tool.save
       redirect_to root_path
     else
