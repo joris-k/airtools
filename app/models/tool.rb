aa8b53576639fea1484da396ed241b2e0fce9da6
Otto Jonetzki <ottojonetzki@gmail.com>
2019-11-19 11:36:46 +0100

    validations

--- app/models/tool.rb
+++ app/models/tool.rb
@@ -2,4 +2,5 @@ class Tool < ApplicationRecord
   belongs_to :user
 
   validates :name, :long, :lat, :date, :price, :category, :sub_category, :description, :created_at, :updated_at, :user_id, presence: true
+  validates :price, numericality: { only_integer: true }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
 
   has_many :tools, dependent: :destroy
   has_many :bookings, dependent: :destroy
+
+  validates :first_name, :last_name, :email, :password, :password_confirmation, :created_at, presence: true
 end
