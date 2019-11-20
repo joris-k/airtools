8784bf44a9df2fb0dd821ee96d94c0d1332cdfad
Otto Jonetzki <ottojonetzki@gmail.com>
2019-11-20 19:22:59 +0100

    new seed and removed long and lat

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_19_152114) do
+ActiveRecord::Schema.define(version: 2019_11_20_181938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,6 @@ ActiveRecord::Schema.define(version: 2019_11_19_152114) do
 
   create_table "tools", force: :cascade do |t|
     t.string "name"
-    t.string "long"
-    t.string "lat"
     t.string "date"
     t.float "price"
     t.string "category"
@@ -54,6 +52,7 @@ ActiveRecord::Schema.define(version: 2019_11_19_152114) do
     t.datetime "updated_at", null: false
     t.string "first_name", default: "", null: false
     t.string "last_name", default: "", null: false
+    t.string "user_photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
