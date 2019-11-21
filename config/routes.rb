0c87cd8dfbe0e7a5b0fbe32e7eb6dd5877c51cbb
Joris Kuehnast <joris@kuehnast.one>
2019-11-21 16:11:40 +0100

    edit function added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :tools, only: [:create, :new, :index, :show, :destroy] do
+  resources :tools, only: [:create, :new, :index, :show, :destroy, :edit, :update] do
     resources :bookings, only: [:new, :create]
   end
   resources :bookings, only: [:show, :index, :update]
