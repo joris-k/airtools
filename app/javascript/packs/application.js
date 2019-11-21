e0e932975edf5ead492a4f45122442bbbcca398d
Paul Ladwig <ladwigpaul@icloud.com>
2019-11-21 20:58:05 +0100

    maps toggle enabled

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,17 @@ import "bootstrap";
 import flatpickr from '../components/datepicker';
 import { initAutocomplete } from "../plugins/init_autocomplete";
 import {buttonActivationProfile} from '../pages/profile.js';
+import {buttonActivationMap} from '../pages/toolsIndex.js';
 import { initSelect2 } from '../components/dropdown';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
-
 import { initMapbox } from '../plugins/init_mapbox';
-import {addLinkInNavbarToggle} from '../pages/home.js';
 
+import {addLinkInNavbarToggle} from '../pages/home.js';
+buttonActivationProfile();
+buttonActivationMap();
 addLinkInNavbarToggle();
 initSelect2();
-initMapbox();
 initAutocomplete();
 
-buttonActivationProfile();
+
--- app/javascript/pages/profile.js
+++ app/javascript/pages/profile.js
@@ -1,23 +1,24 @@
+
 const buttonActivationProfile = () => {
   const bookingButton = document.querySelector('#profile-bookings-button');
   const toolButton = document.querySelector('#profile-tools-button');
   const bookingShow = document.querySelector('#profile-bookings-show');
   const toolShow = document.querySelector('#profile-tools-show');
 
-
-  bookingButton.addEventListener("click", (event) => {
-    bookingButton.classList.toggle("button-unselected");
-    toolButton.classList.toggle("button-unselected");
-    bookingShow.classList.toggle("hidden");
-    toolShow.classList.toggle("hidden");
-  });
-
-  toolButton.addEventListener("click", (event) => {
-    bookingButton.classList.toggle("button-unselected");
-    toolButton.classList.toggle("button-unselected");
-    bookingShow.classList.toggle("hidden");
-    toolShow.classList.toggle("hidden");
-  });
+  if (bookingButton) {
+    bookingButton.addEventListener("click", (event) => {
+      bookingButton.classList.toggle("button-unselected");
+      toolButton.classList.toggle("button-unselected");
+      bookingShow.classList.toggle("hidden");
+      toolShow.classList.toggle("hidden");
+    });
+    toolButton.addEventListener("click", (event) => {
+      bookingButton.classList.toggle("button-unselected");
+      toolButton.classList.toggle("button-unselected");
+      bookingShow.classList.toggle("hidden");
+      toolShow.classList.toggle("hidden");
+    });
+  }
 }
 
 export {buttonActivationProfile};
